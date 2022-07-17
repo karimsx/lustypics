3bbddae16247153c3bd7d4612d15b202f524a7fa
karimsx <karim.squalli.work@gmail.com>
2022-07-17 17:16:30 +0200

    implement email validation

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -1,7 +1,9 @@
 import db from "db"
-import { SecurePassword } from "@blitzjs/auth"
+import { generateToken, hash256, SecurePassword } from "@blitzjs/auth"
 import { Role } from "types"
 import { parseTemplate, sendMail } from "mailers/base/mailer-service"
+
+const VALIDATE_EMAIL_TOKEN_EXPIRATION_IN_HOURS = 1
 
 export default async function signup(input, ctx) {
   const blitzContext = ctx
@@ -18,9 +20,27 @@ export default async function signup(input, ctx) {
     role: user.role as Role,
   })
 
+  // generate and send email validation token
+  const token = generateToken()
+  const hashedToken = hash256(token)
+  const expiresAt = new Date()
+  expiresAt.setHours(expiresAt.getHours() + 1)
+
+  await db.token.deleteMany({ where: { type: "VALIDATE_EMAIL", userId: user.id } })
+
+  await db.token.create({
+    data: {
+      user: { connect: { id: user.id } },
+      type: "VALIDATE_EMAIL",
+      expiresAt,
+      hashedToken,
+      sentTo: user.email,
+    },
+  })
+
   await sendMail({
     mailOptions: {
-      from: "test",
+      from: "no",
       to: "tata@live.fr",
       subject: "tezteztze",
       html: await parseTemplate("welcomeMail.mjml", { name: "Karim Squalli" }),
