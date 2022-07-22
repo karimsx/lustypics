8dbb4ffcd09491a618b1d42a3f9d7c3842511e62
karimsx <karim.squalli.work@gmail.com>
2022-07-22 18:36:42 +0200

    add galleries detail by id design

--- pages/galleries/[id].tsx
+++ pages/galleries/[id].tsx
@@ -17,57 +17,33 @@ import {
   Skeleton,
   Stack,
   Typography,
+  Link as ReactLink,
 } from "@mui/material"
 import { Title } from "@mui/icons-material"
 import PrimaryAppBar from "app/core/components/AppBar"
 import { Box } from "@mui/system"
-
-/*
- * This file is just for a pleasant getting started page for your new app.
- * You can delete everything in here and start from scratch if you like.
- */
-
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  const [logoutMutation] = useMutation(logout)
-
-  if (currentUser) {
-    return (
-      <>
-        <Button
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </Button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Link href={Routes.SignupPage()}>
-          <a className="button small">
-            <strong>Sign Up</strong>
-          </a>
-        </Link>
-        <Link href={Routes.LoginPage()}>
-          <a className="button small">
-            <strong>Login</strong>
-          </a>
-        </Link>
-      </>
-    )
-  }
-}
+import { faker } from "@faker-js/faker"
 
 const Home = () => {
   const cards = [0, 1, 2, 3, 4, 5, 6, 8, 7, 4, 6, 8]
+  const getImagesMock = () => [
+    {
+      original: faker.image.abstract(640, 480, true),
+      thumbnail: faker.image.abstract(640, 480, true),
+    },
+    {
+      original: faker.image.abstract(640, 480, true),
+      thumbnail: faker.image.abstract(640, 480, true),
+    },
+    {
+      original: faker.image.abstract(640, 480, true),
+      thumbnail: faker.image.abstract(640, 480, true),
+    },
+    {
+      original: faker.image.abstract(640, 480, true),
+      thumbnail: faker.image.abstract(640, 480, true),
+    },
+  ]
 
   return (
     <>
@@ -77,40 +53,28 @@ const Home = () => {
             Latest galleries
           </Typography>
 
-          <Grid spacing={3} container>
-            {cards.map((card) => (
-              <Grid item xs={12} md={3}>
-                <Card>
-                  <Skeleton variant="rectangular" height={118} />
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-
-          <Stack mt={2} alignItems={"flex-end"}>
-            <Button variant="contained"> See More</Button>
-          </Stack>
+          {cards.map((el) => (
+            <Grid mb={4} container>
+              {getImagesMock().map((image) => (
+                <Grid item md={3}>
+                  <Link href="/galleries/1" passHref>
+                    <ReactLink>
+                      <Box
+                        sx={{
+                          backgroundImage: `url(${image.original})`,
+                          backgroundOrigin: "center",
+                          backgroundPosition: "center",
+                          height: "300px",
+                          mr: 2,
+                        }}
+                      />
+                    </ReactLink>
+                  </Link>
+                </Grid>
+              ))}
+            </Grid>
+          ))}
         </Box>
-
-        <Box pt={5}>
-          <Typography mb={2} variant="h4" component="h2">
-            Most viewed galleries
-          </Typography>
-
-          <Grid spacing={3} container>
-            {cards.map((card) => (
-              <Grid item xs={12} md={3}>
-                <Card>
-                  <Skeleton variant="rectangular" height={118} />
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-
-        <Stack mt={2} alignItems={"flex-end"}>
-          <Button variant="contained"> See More</Button>
-        </Stack>
       </Container>
     </>
   )
