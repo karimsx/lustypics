fe3971527c0f95c79816001ee3a5681efa5157d6
karimsx <karim.squalli.work@gmail.com>
2022-08-02 14:07:18 +0200

    add get galleries query

--- app/galleries/queries/getGalleries.ts
+++ app/galleries/queries/getGalleries.ts
@@ -1,9 +1,29 @@
 import { Ctx } from "blitz"
 import db from "db"
+import { z } from "zod"
 
-export default async function getGalleries(_ = null, { session }: Ctx) {
+export type FilterType = "lasted" | "most_viewed"
+export const GetGalleriesInput = z.object({
+  page: z.number().int().gte(0),
+  perPage: z.number().int().gt(0),
+  filterType: z.enum(["lasted", "most_viewed"]).optional(),
+})
+
+export default async function getGalleries(
+  { page, perPage, filterType }: z.infer<typeof GetGalleriesInput>,
+  { session }: Ctx
+) {
   const galleries = await db.gallery.findMany({
-    take: 20,
+    take: perPage,
+    skip: page * perPage,
+    orderBy:
+      filterType ?? filterType == "lasted"
+        ? {
+            createdAt: "desc",
+          }
+        : {
+            updatedAt: "desc",
+          },
   })
 
   return galleries
