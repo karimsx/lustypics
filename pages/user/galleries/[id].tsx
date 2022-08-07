d5e8237d9566e6493abb85049f98508f249ea3c4
karim <karim.squalli.work@gmail.com>
2022-08-07 12:40:33 +0200

    add dropzone to gallery editor

--- pages/user/galleries/[id].tsx
+++ pages/user/galleries/[id].tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react"
+import { ReactNode, useCallback, useRef } from "react"
 import Link from "next/link"
 import { Container, Grid, Link as ReactLink, Pagination, Paper } from "@mui/material"
 import { Box } from "@mui/system"
@@ -8,13 +8,14 @@ import dynamic from "next/dynamic"
 import "tui-image-editor/dist/tui-image-editor.css"
 import { faker } from "@faker-js/faker"
 import Scrollbar from "../../../app/core/components/Scrollbar"
+import Dropzone from "react-dropzone"
 
 let ImageEditor: any = dynamic<ReactNode>(() => import("@toast-ui/react-image-editor"), {
   ssr: false
 })
 
 const EditGallery = () => {
-  const getImagesMock = () => [
+  const getImagesMock = useCallback(() => [
     {
       original: faker.image.abstract(640, 480, true),
       thumbnail: faker.image.abstract(640, 480, true)
@@ -31,7 +32,8 @@ const EditGallery = () => {
       original: faker.image.abstract(640, 480, true),
       thumbnail: faker.image.abstract(640, 480, true)
     }
-  ]
+  ], [])
+  const editorRef = useRef(null)
 
   const cards = [0, 1, 2, 3, 4, 5, 6, 8, 7, 4, 6, 8]
   const images = [1, 2, 3, 4]
@@ -40,17 +42,23 @@ const EditGallery = () => {
     "header.display": "none"
   }
 
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+  }, [])
+
+  const handleSelectImage = (evt, image) => {
+    evt.stopPropagation();
+    evt.preventDefault();
+  }
+
   return (
     <>
       <GenericHeader primaryText="Edit gallery" secondaryText="gallery name" />
 
       <Container>
         <ImageEditor
+          ref={editorRef}
           includeUI={{
-            loadImage: {
-              path: "img/sampleImage.jpg",
-              name: "SampleImage"
-            },
             theme: myTheme,
             menu: ["shape", "filter"],
             initMenu: "filter",
@@ -69,39 +77,48 @@ const EditGallery = () => {
           usageStatistics={true}
         />
 
-        <Paper elevation={5}>
+        <Paper sx={{my: 3}} elevation={5}>
           <Scrollbar sx={{ mt: 4, maxHeight: 600 }}>
-            {cards.map((card) => (
-              <Box mt={2}>
-                <Box p={2} mb={3}>
-                  <Grid container>
-                    {getImagesMock().map((image) => (
-                      <Grid item md={3}>
-                        <Link href="/galleries/1" passHref>
-                          <ReactLink>
-                            <Box
-                              sx={{
-                                backgroundImage: `url(${image.original})`,
-                                backgroundOrigin: "center",
-                                backgroundPosition: "center",
-                                height: "300px",
-                                mr: 2
-                              }}
-                            />
-                          </ReactLink>
-                        </Link>
-                      </Grid>
-                    ))}
-                  </Grid>
+            <Dropzone onDrop={onDrop}>
+              {({getRootProps, getInputProps}) => (
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>Drag and drop some files here, or click to select files</p>
 
-                </Box>
-              </Box>
-            ))}
+                  {cards.map((card) => (
+                    <Box mt={2}>
+                      <Box p={2} mb={3}>
+                        <Grid container>
+                          {getImagesMock().map((image) => (
+                            <Grid item md={3}>
+                                  <Box
+                                    onClick={(evt) => handleSelectImage(evt, image.original)}
+                                    sx={{
+                                      backgroundImage: `url(${image.original})`,
+                                      backgroundOrigin: "center",
+                                      backgroundPosition: "center",
+                                      height: "300px",
+                                      mr: 2,
+                                      cursor: "pointer"
+
+                                    }}
+                                  />
+                            </Grid>
+                          ))}
+                        </Grid>
+
+                      </Box>
+                    </Box>
+                  ))}
+                </div>
+              )}
+
+
+            </Dropzone>
+
           </Scrollbar>
 
         </Paper>
-
-
       </Container>
     </>
   )
